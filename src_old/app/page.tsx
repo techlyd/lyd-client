a84d937c299b164dc668d60cd08a134bec12f743
Luiz Dorigo <contato.dorigo@gmail.com>
2023-10-22 01:52:41 -0300

    Delete src_old directory

--- src_old/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { Mock } from "@/components/mock";
-import Home from "./home/page";
-
-export default function App() {
-    return <Mock />;
-}
--- src_old/components/home/content/Other.tsx
+++
@@ -1,7 +0,0 @@
-export const Other = () => {
-    return (
-        <div className="w-full h-screen flex bg-white px-28">
-            <div>Other</div>
-        </div>
-    );
-};
