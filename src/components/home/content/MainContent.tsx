0c4cf1a9d9086ae12ee6ecf4a3ec1cf332d03878
d3vNano <contato.dorigo@gmail.com>
2023-10-25 17:46:16 -0300

    wee...

--- src/components/home/content/MainContent.tsx
+++ src/components/home/content/MainContent.tsx
@@ -9,6 +9,9 @@ import dorigo from "@/assets/img/dorigo.jpg";
 import front from "@/assets/img/front-techs.png";
 import back from "@/assets/img/back-techs.png";
 import webimg from "@/assets/img/web-img.png";
+import porque from "@/assets/img/por-que-somos-diferentes.png";
+import sellphone from "@/assets/img/sellphone.png";
+import lourenzo from "@/assets/img/lourenzo-sell.jpg";
 
 import { BsLinkedin, BsInstagram, BsGithub } from "react-icons/bs";
 
@@ -130,7 +133,13 @@ export const MainContent = () => {
                         </p>
                     </div>
                     <div className="w-1/2 h-full flex items-center justify-center">
-                        <p>img</p>
+                        <Image
+                            src={porque}
+                            alt="logos"
+                            width={550}
+                            height={550}
+                            className="max-w-[50%] absolute"
+                        />
                     </div>
                 </div>
             </section>
@@ -272,60 +281,29 @@ export const MainContent = () => {
                     </div>
                 </div>
             </section>
-            <section>
-                <div className="w-full flex flex-col items-center justify-center gap-4 py-20 bg-[#0B1340]">
-                    <div className="w-full flex flex-col items-center justify-center pb-14">
-                        <div className="text-3xl text-center uppercase pb-4">
+            <section className="w-full h-full flex bg-[#0B1340] text-white py-20">
+                <div className="w-full h-full flex items-center justify-center px-28">
+                    <div className="w-1/2 flex flex-col justify-center gap-4">
+                        <div className="w-full text-3xl font-bold uppercase">
                             <h1 className="text-[#2087e2]">
                                 Onde a lyd deixou sua marca
                             </h1>
-                            <span className="w-full h-[1.5px] flex bg-[#2087e2] my-2"></span>
+                            <span className="w-5/6 h-[1.5px] flex bg-[#2087e2] my-2"></span>
                         </div>
-                        <p className="w-1/3 flex flex-col gap-6 text-sm text-center">
+                        <p>
                             Empresas e projetos que ajudamos a consolidar no
                             mercado e que nos deixam cheios de orgulho.
                         </p>
                     </div>
-                    <div className="w-full flex items-center justify-center">
-                        <div className="w-3/5 flex items-center justify-center">
-                            <div className="h-[200px] flex flex-col items-center justify-center flex-[1] transition-[.3s] hover:flex-[3]">
+                    <div className="w-1/2 h-full flex items-center justify-center">
+                        <div className="w-full h-full flex flex-wrap items-center justify-center gap-10">
+                            <div className="w-fit h-fit flex items-center justify-center bg-[#040a5e3a] rounded-3xl p-10 saturate-0 hover:saturate-100 hover:bg-[#4454ff25] duration-500">
                                 <Image
-                                    src={webimg}
+                                    src={sellphone}
                                     alt="logos"
-                                    width={1000}
-                                    height={1000}
-                                />
-                            </div>
-                            <div className="h-[200px] flex flex-col items-center justify-center flex-[1] transition-[.3s] hover:flex-[3]">
-                                <Image
-                                    src={webimg}
-                                    alt="logos"
-                                    width={1000}
-                                    height={1000}
-                                />
-                            </div>
-                            <div className="h-[200px] flex flex-col items-center justify-center flex-[1] transition-[.3s] hover:flex-[3]">
-                                <Image
-                                    src={webimg}
-                                    alt="logos"
-                                    width={1000}
-                                    height={1000}
-                                />
-                            </div>
-                            <div className="h-[200px] flex flex-col items-center justify-center flex-[1] transition-[.3s] hover:flex-[3]">
-                                <Image
-                                    src={webimg}
-                                    alt="logos"
-                                    width={1000}
-                                    height={1000}
-                                />
-                            </div>
-                            <div className="h-[200px] flex flex-col items-center justify-center flex-[1] transition-[.3s] hover:flex-[3]">
-                                <Image
-                                    src={webimg}
-                                    alt="logos"
-                                    width={1000}
-                                    height={1000}
+                                    width={75}
+                                    height={75}
+                                    className="object-cover"
                                 />
                             </div>
                         </div>
@@ -333,22 +311,55 @@ export const MainContent = () => {
                 </div>
             </section>
             <section className="w-full h-full flex text-white py-20">
-                <div className="w-full h-full flex items-center justify-center px-28">
-                    <div className="w-1/3 flex flex-col justify-center gap-4">
-                        <div className="w-full text-2xl font-bold uppercase">
+                <div className="w-full h-full flex items-center justify-center px-28 gap-10">
+                    <div className="w-1/2 flex flex-col justify-center gap-4">
+                        <div className="w-full text-3xl font-bold uppercase">
                             <h1 className="text-[#2087e2]">
                                 O que dizem sobre a lyd
                             </h1>
                             <span className="w-4/5 h-[1.5px] flex bg-[#2087e2] my-2"></span>
                         </div>
-                        <p>
+                        <p className="w-4/5">
                             A satisfação de nossos clientes é o que realmente
                             importa. Confira a opinião de quem já confiou em
                             nós.
                         </p>
                     </div>
-                    <div className="w-2/3 h-full flex items-center justify-center">
-                        <p>testemunhas e avaliações</p>
+                    <div className="w-1/2 h-full flex items-center justify-center">
+                        <div className="w-full flex flex-col items-start justify-center gap-1">
+                            <div className="w-full flex items-center gap-5">
+                                <div className="w-fit h-full bg-blue-500 rounded-s-full rounded-tr-full p-1">
+                                    <Image
+                                        src={lourenzo}
+                                        alt="logos"
+                                        width={75}
+                                        height={75}
+                                        className="object-cover rounded-s-full rounded-tr-full"
+                                    />
+                                </div>
+                                <div>
+                                    <p className="text-2xl font-bold">
+                                        Rafael Lourenzo
+                                    </p>
+                                    <p className="text-xl font-medium text-gray-500">
+                                        CEO - SellPhone
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="w-fit h-fit bg-blue-500 rounded-e-3xl rounded-bl-3xl p-4 ml-20">
+                                <p className="text-sm font-medium italic">
+                                    " Mesmo investindo consideravelmente em
+                                    recursos no marketing interno, minha empresa
+                                    enfrentava problemas nas vendas online. Ao
+                                    contratar diversas agências de marketing,
+                                    porém, a entrega dos serviços foi
+                                    inconsistente e os problemas persistiam.
+                                    Agora, com os serviços da Lyd, obtenho um
+                                    retorno claro do investimento, excelente
+                                    trabalho. "
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
